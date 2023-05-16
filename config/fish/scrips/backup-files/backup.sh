7b8986a7a72f9019ee596c04f373a7aabc6500f6
√ıямв <102460538+Irmb7108@users.noreply.github.com>
2023-05-16 11:10:51 +0330

    Update backup.sh

--- config/fish/scrips/backup-files/backup.sh
+++ config/fish/scrips/backup-files/backup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-
-echo -e '\033[8;30;'$(tput colors)';120t'
-
-# Checking if 'lolcat' package is installed and proceeding with the installation if missing
+# Setting color codes
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+# Checking if 'rsync' package is installed and proceeding with the installation if missing
 check_dep() {
     local dep=$(which "$1" 2>/dev/null)
     if [[ -z "$dep" ]]; then
-        read -p "The $1 package is not found! Do you want me to install it for you? [y/n]: " choice
+        echo -e "${RED}The $1 package is not found! Do you want me to install it for you? [y/n]:${NC} \c" 
+        read choice
         case "$choice" in 
             y|Y ) 
                 echo "Installing $1 package..."
@@ -18,10 +19,12 @@ check_dep() {
     fi
 }
 
-# Check for 'figlet' package
+# Check for 'rsync', 'figlet', and 'lolcat' packages
+check_dep "rsync"
 check_dep "figlet"
 check_dep "lolcat"
 
+echo -e '\033[8;30;'$(tput colors)';120t'
 echo ""
 echo ""
 figlet -cf smslant "IR-MB" | lolcat
@@ -32,7 +35,7 @@ figlet -cf term "$text"|lolcat
 echo ""
 
 echo "------------------------------------------------------"
-echo -e "\033[48;5;24m\033[38;5;15m  # copy only backup files to backup directory   \033[m"
+echo -e "\033[48;5;24m\033[38;5;15m  # Backup process   \033[m"
 echo "------------------------------------------------------"
 echo ""
 # echo " "
@@ -75,6 +78,7 @@ function spinner {
 
 # create backup directory
 #backup_dir="backup"
+#backup_dir="backup_$(date "+%H%M%S")"
 backup_dir="backup_$(date "+%H.%M.%S")"
 if [ ! -d "$backup_dir" ]; then
   mkdir "$backup_dir"
@@ -82,35 +86,10 @@ fi
 
 # backup directories and files
 echo "Starting backup..."
-for dir in */; do
-  dir=${dir%*/}   
-  # Remove the trailing "/"
-  echo "Backing up $dir..."
-  rsync -a --delete --exclude=".git" "$dir/" "$backup_dir/$dir/"
-done
+rsync -avh --exclude=".git" ./ "$backup_dir/"
 echo "Backup completed."
 
-# copy only backup files to backup directory
-echo "Copying backup files to directory: $backup_dir"
-total_files=$(find . -type f | wc -l)
-progress=-1 
-# so percentage starts from 0
-current_file=0
-copied_files_count=0
-for file in *; do
-  ((current_file++))
-  percentage=$(expr $current_file \* 100 / $total_files)
-  if [[ -f "$file" && $(basename "$file") != "$(basename "$0")" ]]; then
-    cp -rf "$file" "$backup_dir" &
-    tput civis;
-    printf "\rCopying...[%d%%]" $percentage
-    spinner
-    ((copied_files_count++))
-  fi
-done
-echo ""
-filename="backup_$(date "+%H-%M-%S").md"
-cd $backup_dir 
+filename="$backup_dir/backup_$(date "+%H-%M-%S").md"
 touch "$filename"
 echo "# Backup Information" >> "$filename"
 
@@ -118,18 +97,16 @@ echo "" >> "$filename"
 echo "## Backup Time: $(date)" >> "$filename"
 echo "## Backed Up By: $(whoami)" >> "$filename" 
 echo "## Backed Up Folder Name: $backup_dir" >> "$filename"
-echo "## Backed Total files copied: ${copied_files_count}" >>"$filename"
+echo "## Backed Total files copied: $(ls -1 "$backup_dir" | wc -l)" >>"$filename"
 
-# copy requested file to backup directory
-# set the color to blue
 BLUE='\033[0;34m'    
-
-echo "Copy completed."
 echo "The backup directory is: ${backup_dir}"
-printf "${BLUE}Total files copied: ${copied_files_count}${NC}\n"
+printf "${BLUE}Total backup files copied: $(ls -1 "$backup_dir" | wc -l)${NC}\n"
 echo -e '\e[32mThe Backup Process has been completed successfully!\e[0m'
 echo " "
 echo " "
 
 text="Created By IR-MB!"
 figlet -cf term "$text" |lolcat
+
+
